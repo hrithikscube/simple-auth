31a4596d64ce301b5304b53eb4d8f003e6d57ee6
hrithikscube <hrithik@scube.me>
2025-03-26 16:59:16 +0530

    cleanup

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    REACT_BACKEND_URL: process.env.REACT_BACKEND_URL,
-  },
-};
-
-export default nextConfig;
--- src/pages/home/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Home = () => {
-    return (
-        <div className='flex flex-col w-full h-screen items-center justify-center'>Home</div>
-    )
-}
-
-export default Home
